19844d1687985dc6c7a9cf498365e20fada4187c
Luca Garizio <garizioluca2@gmail.com>
2024-02-12 17:35:06 +0100

    add style to page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,11 +3,21 @@
     <title>Home</title>
 @endsection
 @section('content')
-<h1>Lista dei Film</h1>
-<ul>
-    @foreach($movies as $movie)
-        <li>{{ $movie->title }}</li>
-    @endforeach
-</ul>
-</body>
+<div class="container-fluid py-5">
+    <h1 class="text-center text-white mb-5">Lista dei Film</h1>
+    <div class="row justify-content-center">
+       
+        @foreach($movies as $movie)
+        <div class="col-2 bg-success text-white border mx-2 my-2 " style="height: 200px;">
+            <ul class="text-start p-0">
+                <li><h5>Nome: {{ $movie->title }}</h5></li>
+                <li><h6>Paese: {{ $movie->nationality }}</h6> </li>
+                <li><h6>Data: {{ $movie->date }}</h6></li>
+                <li><h6>Vote: {{ $movie->vote }}</h6></li>
+            </ul>
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
+
